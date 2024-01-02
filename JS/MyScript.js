bb06dac9941783a590eab52a6dd0f348a37f18a3
Mariam <maremrashad@gmail.com>
2024-01-02 20:14:27 +1100

    weather

--- JS/MyScript.js
+++ JS/MyScript.js
@@ -3,18 +3,46 @@
 const logIn =  document.getElementById('logIn');//log in button 
 const Name=document.getElementById('Name');//name
 const Password= document.getElementById('Password')//password
+const check =  document.getElementById('Check');//log in button 
+
+//welcome function
+function welcomeUserName()
+{
+ // console.log( document.getElementById('userName').value)
+try {
+ // alert(document.getElementById('error').value)
+ window.onload = (event) => {
+  console.log("page is fully loaded");
+};
+L
+  // var userName = localStorage.getItem('userName',Name.value);
+
+  // var inputName = localStorage['userName'];
+  // var displayName = inputName;
+
+  // document.getElementById('WelcomeUser').innerText="Welcome "+displayName;
+    } 
+    
+    catch (error) {
+}}
 
 function checkInput()// function to check the text box input not empty
 {
 	
   try {
+
     document.getElementById('error').innerText="";//empty error message
 
     if(Name.value !="" && Password.value !="" )//mycondition
-    
-    
    {
-      document.getElementById('error').innerText=Name.value+Password.value;//goto wether page
+
+    window.open("./CheckWeather.html");        //goto wether page
+
+     //welcomeUserName();
+
+     document.getElementById('Name').value='';//empty user name
+     document.getElementById('Password').value='';//empty epassword
+
      
     }
   else
@@ -28,7 +56,10 @@ function checkInput()// function to check the text box input not empty
  
   }
 }
-
-logIn.addEventListener('click', checkInput)//click logIn button event
+try{
+logIn.addEventListener('click', checkInput);//click logIn button event
+}catch
+{}
+check.addEventListener('click',welcomeUserName);
 
 
